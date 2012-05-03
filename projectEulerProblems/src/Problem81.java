fb1f41bd55c582305b02c8736a7332e9b9d66c39
Boris Shminke <schminkeba@gmail.com>
2012-05-03 23:35:46 +0400

    Very simple version of Dijkstra's algorithm

--- projectEulerProblems/src/Problem81.java
+++ projectEulerProblems/src/Problem81.java
@@ -20,10 +20,26 @@ public class Problem81 {
 	}
 
 	public static long solve () {
-		String filename = "test_matrix.txt";
-		int n = 5;
+		String filename = "matrix.txt";
+		int n = 80;
 		long[][] matrix = new long[n][n];
 		readMatrix(matrix, filename, n, n);
-		return 0;
+		long[][] mmatrix = new long[n][n];
+		mmatrix[0][0] = matrix[0][0];
+		for (int i = 1; i < n; i ++) {
+			mmatrix[i][0] = mmatrix[i - 1][0] + matrix[i][0];
+			mmatrix[0][i] = mmatrix[0][i - 1] + matrix[0][i];
+		}
+		for (int i = 1; i < 2 * n; i ++) {
+			int j = i - n + 1;
+			if (j < 1) j = 1;
+			for (; j < i && j < n; j ++) {
+				if (mmatrix[j - 1][i - j] > mmatrix[j][i - j - 1])
+					mmatrix[j][i - j] = mmatrix[j][i - j - 1] + matrix[j][i - j];
+				else
+					mmatrix[j][i - j] = mmatrix[j - 1][i - j] + matrix[j][i - j];
+			}
+		}
+		return mmatrix[n - 1][n - 1];
 	}
 }
