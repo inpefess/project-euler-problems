6d3e06cab1933616d8b6a2623bf95691b7e95105
inpefess <schminkeba@gmail.com>
2012-05-04 10:49:04 +0400

    The problem was named 241 by mistake

---
+++ projectEulerProblems/src/Problem243.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Problem243 {
+	public static long solve () {
+		ArrayList<Long> primes = new ArrayList<Long>(1000000);
+		int r = 15499;
+		int d = 94744;
+		for (long nextNum = 2; nextNum < 1000000; nextNum ++) {
+			long remainder = nextNum;
+			long phi = nextNum;
+			for (int i = 0; i < primes.size() && remainder > 1; i ++) {
+				if (remainder % primes.get(i) == 0) {
+					phi = (phi * (primes.get(i) - 1)) / primes.get(i);
+				}
+				while (remainder % primes.get(i) == 0)
+				{
+					remainder /= primes.get(i);
+				}
+			}
+			if (remainder > 1) {
+				primes.add(nextNum);
+				phi = nextNum - 1;
+			}
+			if 	(phi * d < r * (nextNum - 1)) return nextNum;
+		}
+		return 0;
+	}
+}
