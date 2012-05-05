710196b490ba33b7e629ac22975e14bea92e11ad
inpefess <schminkeba@gmail.com>
2012-05-05 18:30:20 +0400

    Solution to problem 92 added

---
+++ projectEulerProblems/src/Problem92.java
@@ -0,0 +1,27 @@
+
+public class Problem92 {
+	private static int digitSquareSum(int j) {
+		String line = String.valueOf(j);
+		int k = 0;
+		for (int i = 0; i < line.length(); i ++) {
+			int m = Integer.valueOf(String.valueOf(line.charAt(i)));
+			k += m * m;
+		}
+		return k;
+	}
+	
+	public static int solve () {
+		int n = 0;
+		int percent = 0;
+		for (int i = 2; i < 10000000; i ++) {
+			if (i / 10000 > percent) {
+				percent ++;
+				System.out.println(percent);
+			}
+			int j;
+			for(j = i; j != 1 && j != 89; j = digitSquareSum(j));
+			if (j == 89) n ++;
+		}
+		return n;
+	}
+}
