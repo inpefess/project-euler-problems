98d1283f4eaf2ebe3139cf1ba27fafc4be92ec22
inpefess <schminkeba@gmail.com>
2012-05-05 18:34:33 +0400

    Fixed some bugs

--- projectEulerProblems/src/Problem65Test.java
+++ projectEulerProblems/src/Problem65Test.java
@@ -9,7 +9,7 @@ public class Problem65Test {
 
 	@Test
 	public void test() {
-		double[] testCont = {3, 8/3., 11/4., 19/7., 8/32., 106/39., 193/71., 1264/465., 1457/536.};
+		double[] testCont = {3, 8/3., 11/4., 19/7., 87/32., 106/39., 193/71., 1264/465., 1457/536.};
 
 		for (int i = 0; i < 9; i ++) {
 			BigInteger[] x = Problem65.getCont(i + 1);
