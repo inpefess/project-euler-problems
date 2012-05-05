b26fb9114c529311bf6fa6a184359a81a6603182
Boris Shminke <schminkeba@gmail.com>
2012-05-05 09:07:17 +0400

    Solution to 243 was upgraded, but still is wrong

--- projectEulerProblems/src/Problem243.java
+++ projectEulerProblems/src/Problem243.java
@@ -1,21 +1,14 @@
 import java.util.ArrayList;
 
 public class Problem243 {
-	public static int maxNum = 1299709;
-	public static ArrayList<Integer> eulerPhi = new ArrayList<Integer>(1000000);
+	public static ArrayList<Integer> eulerPhi;
 	
-	public static int solve () {
-		ArrayList<Integer> primes = new ArrayList<Integer>(1000000);
+	public static int solve (int maxNum, int r, int d) {
+		eulerPhi = new ArrayList<Integer>(maxNum);
+		ArrayList<Integer> primes = new ArrayList<Integer>(maxNum);
 		PrimeNumbers.loadPrimes(primes, maxNum, "PrimeNumbers.txt");
 		eulerPhi.clear();
-		int r = 15499;
-		int d = 94744;
-		int percent = 0;
 		for (int nextNum = 2; nextNum < maxNum; nextNum ++) {
-			if ((nextNum * 100) / maxNum > percent) {
-				percent ++;
-				System.out.println(percent);
-			}
 			int phi = 1;
 			int k = primes.get(0);
 			int reminder = nextNum;
