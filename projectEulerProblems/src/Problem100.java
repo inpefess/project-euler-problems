27892bbbb325b3f0db752f482e784b5226daae13
Boris Shminke <schminkeba@gmail.com>
2012-05-11 00:58:09 +0400

    At least now it fits the tests

--- projectEulerProblems/src/Problem100.java
+++ projectEulerProblems/src/Problem100.java
@@ -1,10 +1,15 @@
 public class Problem100 {
 	public static long solve (long n) {
 		for (long total = n; ; total ++) {
-			double rnd = 0.5 * (1 + Math.sqrt(2 * total * total - 2 * total + 1));
-			for (long blue = (long) (rnd * 0.9); blue < rnd * 1.1; blue ++)
-			if ((total * (total - 1) % (blue % (blue - 1)) == 0) ) {
-				return blue;
+			double rnd = 0.5 * (1.0 + Math.sqrt(2.0 * (double) total * (double) total - 2.0 * (double) total + 1.0));
+			long min = (long) (rnd * 0.999999999) - 1;
+			long max = (long) (rnd * 1.000000001) + 1;
+			for (long blue = min; blue <= max ; blue ++) {
+				if ((total * (total - 1) % (blue * (blue - 1)) == 0)) {
+					if ((total * (total - 1) / (blue * (blue - 1)) == 2)) {
+							return blue;
+					}
+				}
 			}
 		}
 	}
